815514d3e9e7c019beea228e7e674e8c97490fd7
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-13 15:46:08 -0300

    add changes

--- simpleExamples/PythonCode-3-3.py
+++ simpleExamples/PythonCode-3-3.py
@@ -1,10 +1,13 @@
 #hours, minutes and seconds
 from time import sleep, strftime
 
-while True: #will run forever
-    hora = strftime('%H:%M:%S')
-    print(hora)
-    sleep(1) #wait 1 second
+#while True: #will run forever
+#    hora = strftime('%H:%M:%S')
+#    print(hora)
+#    sleep(1) #wait 1 second
+n = 0
 
-    
+while n < 10:
+    print(n)
+    n = n + 1
     
--- simpleExamples/PythonCode-3-4-2.py
+++ simpleExamples/PythonCode-3-4-2.py
@@ -28,3 +28,4 @@ d['xyz'] = 123
 d['abc'] = 345
 for i in d:
     print("%s  %d" %(i, d[i]))
+
