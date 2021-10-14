79f3e9169f88263851f7b3db0f3e83b296ff8466
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-14 13:50:47 -0300

    exe taboada

---
+++ simpleExamples/PythonCodeTaboada.py
@@ -0,0 +1,5 @@
+#tabuada do número 2 ao 5
+for num_coluna1 in range(2, 11):
+    print("Tabuada do nº", num_coluna1)
+    for num_coluna2 in range(11):
+        print(num_coluna1, "x", num_coluna2, " = ", num_coluna1 * num_coluna2)
