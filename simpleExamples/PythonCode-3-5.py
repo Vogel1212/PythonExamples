03649d9ef90c96b1b3ea435dbbe4c26c0b10ef0a
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-14 13:35:13 -0300

    add exe

---
+++ simpleExamples/PythonCode-3-5.py
@@ -0,0 +1,19 @@
+#studying while and for
+#
+# escreve um programa com 3 variables, var1 = um pequeno numero, var2 um numero maior, var3 tambem pequeno. 
+# faz um loop ( pode ser while ou for) imprimir var1 (print) . addicionar var1 com var3. 
+# conitinar ate var1 nao mais é menor que var2. output por variables 3,20,2 ( usa outros numeros, isso é meu exemplo) : 3,5,7,9,11,13,15,1
+#
+#exemplo desafio 1
+
+var1,var2,var3 = 3,20,2
+while var1 < var2:
+    print(var1)
+    var1 = var1 + var3
+
+
+#o inverso
+#var1,var2,var3 = 20,3,2
+#while var1 > var2:
+#    print(var1)
+#    var1 = var1 - var3
---
+++ simpleExamples/PythonCode-3-6.py
@@ -0,0 +1,8 @@
+#studying while and for
+#
+# escreve um programa com 2 variables, var1 = um pequeno numero, var2 um numero maior, var3 tambem pequeno. 
+# faz um loop ( pode ser while ou for) imprimir var1 (print) se pode dividir com var3. caso nao, nao imprimir nada.  
+# addicionar var1 com 1 ( nao var3). continuar ate var1 nao mais é menor que var2. output pra variables 1,20,3 ( usa outros numers, isso é meu exemplo) : 3,6,9,12,15,18
+#
+#exemplo desafio 2
+
