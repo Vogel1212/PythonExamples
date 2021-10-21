2419a3f9be0ffb724a296eaef23c50c75b7db8bf
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-21 15:58:17 -0300

    add example

---
+++ moduleExamples/PythonCodeChallenge-2-8.py
@@ -0,0 +1,16 @@
+# course challenge 20
+# O mesmo professor do desafio 019 quer sortear a ordem de apresentação de trabalhos dos alunos. 
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random
+
+a = str(input('Primeiro aluno: '))
+b = str(input('Segundo aluno: '))
+c = str(input('Terceiro aluno: '))
+d = str(input('Quarto aluno: '))
+
+lista = [a,b,c,d]
+random.shuffle(lista)
+
+print('A ordem de apresentação será:')
+print(lista)
