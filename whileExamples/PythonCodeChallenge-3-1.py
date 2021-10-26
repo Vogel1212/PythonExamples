9271fe97276272b401b6e0863d60aa4060a4253a
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-26 09:27:46 -0300

    add changes

--- whileExamples/PythonCodeChallenge-3-1.py
+++ whileExamples/PythonCodeChallenge-3-1.py
@@ -1,4 +1,22 @@
 # course challenge 58
 # Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. 
 # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
-# https://www.youtube.com/watch?v=-QkOIHJ1Chw&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=74
+
+from random import randint
+comp = randint(0, 10)
+print('Hi, Im your computer... I just thought of a number between 0 and 10.')
+print('Can you guess which one it was?')
+answer = False
+guess = 0
+while not answer:
+    player = int(input('Whats your guess? '))
+    guess += 1
+    if player == comp:
+        answer = True
+    else:
+        if player < comp:
+            print('More... Try again!')
+        elif player > comp:
+            print('Less... Try again!')
+print('Got it right with {} attempts. Congratulations!'.format(guess))
+print('Number I thought was {}'.format(comp))
