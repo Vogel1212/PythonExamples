15d523b4dca21b8ba5aabd1b0b372790ee167929
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-26 15:01:15 -0300

    m changes

--- whileExamples/PythonCodeChallenge-3-5.py
+++ whileExamples/PythonCodeChallenge-3-5.py
@@ -1,7 +1,24 @@
 # course challenge 62
 # Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
-        
-        
+
+#print('TERMS OF A ARITHMETIC PROGRESSION')
+#print('-=' * 10)
+#first = int(input('First Term: '))
+#reason = int(input('Reason of A.P: '))
+#term = first
+#cont = 1
+#total = 0
+#more = 10
+#while more != 0:
+#    total = total + more
+#    while cont <= total:
+#        print('{} → '.format(term), end='')
+#        term += reason
+#        cont += 1
+#    print('BREAK')
+#    mais = int(input('How many terms do you want to show more? '))
+#print('Progression ended with {} terms shown'.format(total))
+
 a = int(input('First Term: '))
 b = int(input('Reason of A.P: '))
 c = int(input('Enter the amount of terms of the A.P: '))
--- whileExamples/PythonCodeChallenge-3-6.py
+++ whileExamples/PythonCodeChallenge-3-6.py
@@ -1,4 +1,4 @@
 # course challenge 63
 # Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
 # Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8
-# https://www.youtube.com/watch?v=w7yn1_Mfu0E&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=79
+
