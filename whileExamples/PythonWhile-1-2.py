f2063aaf84c401c0262a554d60093f7018d560a3
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-25 21:32:04 -0300

    add commits

---
+++ whileExamples/PythonWhile-1-2.py
@@ -0,0 +1,8 @@
+passw='1212'
+att=input("Type the password:")
+
+while (passw != att):
+    print("Wrong password! Try again!")
+    att=input("Type the password:")
+
+print("Correct password. Entering the system...")
