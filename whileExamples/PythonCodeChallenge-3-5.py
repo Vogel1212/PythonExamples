6e82037eabf6b7343b96c623ebb560debb1e3dc6
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-26 14:35:40 -0300

    m changes

--- whileExamples/PythonCodeChallenge-3-5.py
+++ whileExamples/PythonCodeChallenge-3-5.py
@@ -1,3 +1,22 @@
 # course challenge 62
 # Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
-# https://www.youtube.com/watch?v=BWAWq7n6PCk&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=78
+        
+        
+a = int(input('First Term: '))
+b = int(input('Reason of A.P: '))
+c = int(input('Enter the amount of terms of the A.P: '))
+d = a
+while d != (a+(b*c)):
+    print(f'{d} → ', end='')
+    d += b
+print('BREAK')
+x = 1
+while x != 0:
+    x = int(input('How many terms do you want to show more?: '))
+    while d != (a+(b*(c+x))):
+        print(f'{d} → ', end='')
+        d += b
+    c += x
+    if x != 0:
+        print('BREAK')
+print(f'Progression ended with {((d-a)/b):.0f} terms shown.')
