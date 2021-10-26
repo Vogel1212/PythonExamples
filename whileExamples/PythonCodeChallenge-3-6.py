d0530b316d3985ad26cea4055adb5ba3af907141
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-26 15:15:12 -0300

    m changes

--- whileExamples/PythonCodeChallenge-3-6.py
+++ whileExamples/PythonCodeChallenge-3-6.py
@@ -2,3 +2,21 @@
 # Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
 # Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8
 
+print('-' * 20)
+print('Fibonacci Sequence')
+print('-' * 20)
+
+terms = int(input('How many terms do you want to show? '))
+cont = 1
+ant = 0
+prox = 1
+sum = 1
+
+print('~'*40)
+while cont <= terms:
+    print(ant, end='-')
+    cont += 1
+    sum = prox + ant
+    ant = prox
+    prox = sum
+print('The end.')
