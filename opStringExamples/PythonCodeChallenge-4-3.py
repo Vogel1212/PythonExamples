a1587a20e950bcbc81056f3be5c19a3425e39ae3
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-27 14:30:33 -0300

    Update PythonCodeChallenge-4-3.py

--- opStringExamples/PythonCodeChallenge-4-3.py
+++ opStringExamples/PythonCodeChallenge-4-3.py
@@ -1,2 +1,7 @@
 # course challenge 26
 # Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+ph = str(input('Digite uma frase:')).upper().strip()
+print('A letra A aparece {} vezes na frase.'.format(ph.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(ph.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(ph.rfind('A')+1))
