48066273f95b186c2860f8faf880031346b20e87
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-27 12:12:21 -0300

    m changes

--- opStringExamples/PythonCodeChallenge-4-1.py
+++ opStringExamples/PythonCodeChallenge-4-1.py
@@ -1,2 +1,5 @@
 # course challenge 24
 # Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+
+city = str(input('Em que cidade você nasceu? ')).strip()
+print(city[:5].upper() == 'SANTO')
--- opStringExamples/PythonCodeChallenge-4-2.py
+++ opStringExamples/PythonCodeChallenge-4-2.py
@@ -1,2 +1,6 @@
 # course challenge 25
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+name = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome tem Silva? ', end='')
+print('silva' in name.lower())
