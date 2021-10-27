651497d7a8d9bcec3dd71ce60f42c4bc7f3b0ac5
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-27 15:13:46 -0300

    m changes

--- opStringExamples/PythonCodeChallenge-4-3.py
+++ opStringExamples/PythonCodeChallenge-4-3.py
@@ -1,7 +1,7 @@
 # course challenge 26
 # Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
 
-ph = str(input('Digite uma frase:')).upper().strip()
-print('A letra A aparece {} vezes na frase.'.format(ph.count('A')))
-print('A primeira letra A apareceu na posição {}'.format(ph.find('A')+1))
-print('A última letra A apareceu na posição {}'.format(ph.rfind('A')+1))
+ph = str(input('Type a phrase:')).upper().strip()
+print('The letter A appears {} times in the sentence.'.format(ph.count('A')))
+print('The first letter A appears in position {}'.format(ph.find('A')+1))
+print('The last letter A appears in the position {}'.format(ph.rfind('A')+1))
--- opStringExamples/PythonCodeChallenge-4-4.py
+++ opStringExamples/PythonCodeChallenge-4-4.py
@@ -1,2 +1,8 @@
 # course challenge 27
 # Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+name = str(input('Enter your full name: '))
+n = name.split()
+print('Nice to meet you!')
+print('Your first name is {}'.format(n[0]))
+print('Your last name is {}'.format(n[len(n)-1]))
