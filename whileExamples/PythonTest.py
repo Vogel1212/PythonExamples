4d1f5740537d638afc36d3c649935453e456935c
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-28 15:27:33 -0300

    m changes

--- whileExamples/PythonTest.py
+++ whileExamples/PythonTest.py
@@ -14,13 +14,11 @@ for i1 in range(2,5):
 
 print('~'*30)
 
-n1,n2=2,1
-while n1 < 5 :
- while n2 < 4:
+n1,n2=1,2
+while n1 < 4 :
+ while n2 < 5:
     print ('n1={} and n2={}'.format(n1,n2))
     n2+=1
- n2=1
- n1+=1
-
- while i1 <4 and i2 <2 
+ n1=1
+ n2+=1
     
