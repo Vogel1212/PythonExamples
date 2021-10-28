a5b2cd7d6865bdd6fcfdaabb96ed8973b592db20
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-10-28 15:08:17 -0300

    add test

---
+++ whileExamples/PythonTest.py
@@ -0,0 +1,26 @@
+i1,i2=2,1
+while i1<5:
+ while i2<4:
+    print('i1={} and i2={}'.format(i1,i2))
+    i2 += 1
+i2=1
+i1+=1
+
+print('~'*30)
+
+for i1 in range(2,5):
+ for i2 in range(1,4):
+      print ('i1={} and i2={}'.format(i1,i2))
+
+print('~'*30)
+
+n1,n2=2,1
+while n1 < 5 :
+ while n2 < 4:
+    print ('n1={} and n2={}'.format(n1,n2))
+    n2+=1
+ n2=1
+ n1+=1
+
+ while i1 <4 and i2 <2 
+    
