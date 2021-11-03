24702b6d3f95c43417ee796e62f8472e2a9d42f2
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-11-03 09:06:35 -0300

    m examples

---
+++ whileExamples/PythonCodeChallenge-4-0.py
@@ -0,0 +1,8 @@
+sum = cont = 0
+while True:
+    num = int(input('Digite um valor (999 para parar): '))
+    if num == 999:
+        break
+    cont += 1
+    sum += num
+print(f'A soma dos {cont} valores foi {sum}')
