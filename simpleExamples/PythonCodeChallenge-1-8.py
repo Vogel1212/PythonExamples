2ffb7636429dd6ac29c0e7befbac79645bfd8b44
Gabriel Vogel <vogel.gabriel@hotmail.com>
2022-01-07 02:00:14 -0300

    Update PythonCodeChallenge-1-8.py

--- simpleExamples/PythonCodeChallenge-1-8.py
+++ simpleExamples/PythonCodeChallenge-1-8.py
@@ -1,9 +1,9 @@
 # course challenge 12
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-price = float(input('How much does the product cost? US$'))
+price = float(input('How much does the product cost? US$' ))
 percent = int(input('How many percent off?: '))
 calc = (price/100)*percent
 sum = price-calc
-print('The product that cost {:.2f}, with the discount coupon from {}% it will cost US${:.2f}'.format(price,percent,sum))
-print('You won US${:.2f}, discount'.format(price-sum))
+print('The product that cost {:.2f}, with the discount coupon from {}% it will cost US$ {:.2f}'.format(price,percent,sum))
+print('You won US$ {:.2f}, discount'.format(price-sum))
