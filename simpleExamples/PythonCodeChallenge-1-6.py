8717b03ddd08c0b12bcc8bbf51437aab88633846
Gabriel Vogel <vogel.gabriel@hotmail.com>
2022-01-07 01:58:32 -0300

    updating dollar

--- simpleExamples/PythonCodeChallenge-1-6.py
+++ simpleExamples/PythonCodeChallenge-1-6.py
@@ -2,6 +2,6 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
 quant = float(input('How much money do you have in your wallet? R$'))
-dolar = 5.58
+dolar = 5.68
 
 print('With R${:.2f} you can buy US${:.2f}'.format(quant, quant / dolar))
